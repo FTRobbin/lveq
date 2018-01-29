c5e11e3fdfa862a47752c026ecccc43017e340bb
Necrololicon <haobinni@163.com>
2018-01-29 12:52:56 -0500

    Main procedure now passes tests

---
+++ src/DAG.hs
@@ -0,0 +1,71 @@
+module DAG where
+
+import Data.List
+
+-- | DAG type definition and DAG operations
+
+-- | record one's precendents
+type Dag = [[Int]]
+
+-- | Edge list to DAG (1 base)
+transform :: Int -- Number of vertices
+             -> [(Int, Int)] -- List of directed edges
+             -> Dag -- Resulted graph
+transform n edge = map (`extract` edge) $ [1..n]
+        where extract i = sort . map fst . filter ((== i) . snd)
+
+-- | topological sort for Dags
+toposort :: Dag -> [Int]
+toposort g =  __topo . map (+ 1) . findIndices null $ g
+    where __topo [] = []
+          __topo l = l ++ (toposort . map (elim l) $ g)
+          elim l [] = [0]
+          elim l l' = filter (`notElem` l) l'
+
+-- | MatchTree records the matches
+data MatchTree = LeafM | NodeM Int Int [MatchTree]
+
+-- | Dumb way to build a map out of two lists
+mapI :: [Int] -> [Int] -> Int -> Int
+mapI la lb a = 
+    case (findIndex (== a) la) of
+        Just i -> lb !! i
+        Nothing -> head lb
+
+-- | Dumb way to erase empty trees
+succMatch :: MatchTree -> Bool
+succMatch (LeafM) = True 
+succMatch (NodeM _ _ l) = or $ map succMatch l
+
+-- | Main matching procedure
+dagMatch :: Dag -> Dag -> Int -> [Int] -> [Int] -> [MatchTree]
+dagMatch gL gR i oL oR =
+    if i == length oL then
+        [LeafM]
+    else
+        let
+            u = oL !! i
+            -- Oh no this did not consider the order of the list!
+            -- But they are pre-sorted
+            rev = map (+ 1) $ findIndices (== (sort (map (mapI (take i oL) oR) (gL !! (u - 1))))) $ gR
+            __dagMatch v = NodeM u v (dagMatch gL gR (i + 1) oL (oR ++ [v]))
+        in
+            filter succMatch $ map __dagMatch rev
+    
+
+-- | Interface
+dagEqv :: Dag -> Dag -> Bool
+dagEqv gL gR =
+    if length gL == length gR then
+        not . null $ dagMatch gL gR 0 (toposort gL) []
+    else
+        False
+
+-- leavesEqv :: [Leaf] -> [Leaf] -> Bool
+
+-- leafPairEqv :: Leaf -> Leaf -> Bool
+
+-- dagEqv :: Dag -> Dag -> (Int -> Int -> Bool) -> Bool
+
+-- dagEqv :: Dag -> Dag -> Bool
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -1,7 +1,49 @@
 module Main where
 
 import Test.HUnit
+import DAG
 
-tests = TestList []
+tr1 = TestCase (assertEqual "Transform1" (transform 2 [(1, 2)]) [[], [1]])
+tr2 = TestCase (assertEqual "Transform2" (transform 4 [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]) [[], [1], [1, 2], [1, 2, 3]])
+tr3 = TestCase (assertEqual "Transform3" (transform 3 []) [[], [], []])
+tr4 = TestCase (assertEqual "Transform4" (transform 5 [(5, 3), (5, 1), (3, 1), (4, 2)]) [[3, 5], [4], [5], [], []])
+tr5 = TestCase (assertEqual "Transform5" (transform 6 [(2, 4), (2, 6), (1, 5), (3, 6), (5, 6)]) [[], [], [], [2], [1], [2, 3, 5]])
+
+transTests = TestList [tr1, tr2, tr3, tr4, tr5]
+
+tp1 = TestCase (assertEqual "Toposort1" (toposort [[], [1]]) [1, 2])
+tp2 = TestCase (assertEqual "Toposort2" (toposort [[], [1], [1, 2], [1, 2, 3]]) [1, 2, 3, 4])
+tp3 = TestCase (assertEqual "Toposort3" (toposort [[], [], []]) [1, 2, 3])
+tp4 = TestCase (assertEqual "Toposort4" (toposort [[5, 3], [4], [5], [], []]) [4, 5, 2, 3, 1])
+tp5 = TestCase (assertEqual "Toposort5" (toposort [[], [], [], [2], [1], [2, 3, 5]]) [1, 2, 3, 4, 5, 6])
+
+topoTests = TestList [tp1, tp2, tp3, tp4, tp5]
+
+dag1 = transform 2 [(1, 2)]
+dag2 = transform 2 [(2, 1)]
+dag3 = transform 3 [(1, 2), (2, 3)]
+dag4 = transform 3 [(1, 2), (3, 2)]
+dag5 = transform 3 [(2, 1), (2, 3)]
+dag6 = transform 3 [(2, 1), (3, 1)]
+dag7 = transform 5 []
+dag8 = transform 5 []
+dag9 = transform 4 [(1, 2), (2, 3), (3, 4), (1, 3)]
+dag10 = transform 4 [(4, 3), (3, 2), (2, 1), (4, 2)]
+dag11 = transform 4 [(4, 3), (3, 2), (2, 1), (4, 2), (4, 1)]
+
+mt1 = TestCase (assertEqual "dagEqv1" (dagEqv dag1 dag2) True)
+mt2 = TestCase (assertEqual "dagEqv2" (dagEqv dag2 dag1) True)
+mt3 = TestCase (assertEqual "dagEqv3" (dagEqv dag1 dag3) False)
+mt4 = TestCase (assertEqual "dagEqv4" (dagEqv dag3 dag4) False)
+mt5 = TestCase (assertEqual "dagEqv5" (dagEqv dag3 dag5) False)
+mt6 = TestCase (assertEqual "dagEqv6" (dagEqv dag4 dag5) False)
+mt7 = TestCase (assertEqual "dagEqv7" (dagEqv dag6 dag4) True)
+mt8 = TestCase (assertEqual "dagEqv8" (dagEqv dag7 dag8) True)
+mt9 = TestCase (assertEqual "dagEqv9" (dagEqv dag9 dag10) True)
+mt10 = TestCase (assertEqual "dagEqv10" (dagEqv dag9 dag11) False)
+
+matchTests = TestList [mt1, mt2, mt3, mt4, mt5, mt6, mt7, mt8, mt9, mt10]
+
+tests = TestList [transTests, topoTests, matchTests]
 
 main = runTestTT tests
